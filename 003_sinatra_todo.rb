3f32cb259f585729aea25906bd944206534689c7
Armen Rostamian <arostamian@ulive.com>
2016-06-27 10:08:20 -0400

    classical vs modular sinatra app

---
+++ 003_sinatra_todo.rb
@@ -0,0 +1,25 @@
+# todo.rb
+# require 'sinatra'
+#
+# get '/' do
+#   'Hello, world!'
+# end
+
+# you notice that Sinatra has some sane defaults, and doesn't need as much code as Rack wants
+# you just ran this with Ruby
+# "Classic style" sinatra app
+
+
+# Modular style below
+
+require 'sinatra/base'
+
+class ToDoApp < Sinatra::Base
+  get '/' do
+    'Hello, world!'
+  end
+end
+
+#config.ru
+require './todo'
+run ToDoApp
